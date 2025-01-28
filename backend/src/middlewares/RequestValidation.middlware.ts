67e26e8f5fb4810911b75a930e1963eaeb6ca233
Elmoatassimm <mouatassemben2004@gmail.com>
2025-01-28 20:43:00 +0100

    feat(auth): add functional authentication middleware

---
+++ backend/src/middlewares/RequestValidation.middlware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema, ZodError } from "zod";
+import { container } from "../config/container";
+import ResponseUtils from "../utils/response.utils";
+
+const responseUtils = container.resolve(ResponseUtils);
+
+
+export const validateRequest =
+  (schema: ZodSchema) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      schema.parse(req.body); // Parse the request body using the schema
+      next();
+    } catch (error: any) {
+      if (error instanceof ZodError) {
+        // Map the Zod errors to a custom error structure with 'field' and 'message'
+        const formattedErrors = error.errors.map((err: any) => ({
+          field: err.path.join("."), // Join the path if it's a nested field
+          message: err.message,
+        }));
+
+        // Send structured validation error response with custom format
+        responseUtils.sendValidationError(
+          res,
+          "Validation failed",
+          formattedErrors 
+        );
+      } else {
+        next(error); 
+      }
+    }
+  };
---
+++ backend/src/middlewares/auth.middlware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { container } from "../config/container";
+import ResponseUtils from "../utils/response.utils";
+import JwtUtils from "../utils/jwt.utils";
+
+const jwt = container.resolve(JwtUtils);
+const responseUtils = container.resolve(ResponseUtils);
+
+export default async function authMiddleware(
+  req: Request & { user?: any },
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      responseUtils.sendUnauthorizedResponse(res);
+      return;
+    }
+
+    const user = await jwt.getUserFromToken(token);
+
+    
+
+    req.user = user; 
+    next(); 
+  } catch (error: any) {
+    responseUtils.sendErrorResponse(res, error.message); // Handle the error
+  }
+}
