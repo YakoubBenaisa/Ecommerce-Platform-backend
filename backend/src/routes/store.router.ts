59ee0cf13296d2956fb325a797cc2c3b1672642b
Elmoatassimm <mouatassemben2004@gmail.com>
2025-01-29 22:23:04 +0100

    feat: add store.router.ts for store management routes

---
+++ backend/src/routes/store.router.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from "express";
+import { container } from "../config/container";
+import StoreController from "../controllers/store.controller";
+
+const storeRouter = Router();
+const storeController = container.resolve(StoreController);
+
+storeRouter.post(
+          "/",
+
+          (req: Request, res: Response):Promise<any> => storeController.createStore(req, res);
+);
+
+storeRouter.put(
+          "/:id",
+
+          (req: Request, res: Response):Promise<any> => storeController.updateStore(req, res);
+);
+
+storeRouter.get(
+          "/:id",
+          (req: Request, res: Response):Promise<any> => storeController.getStoreById(req, res);
+);
+
+export default storeRouter; 
